fb2871a00e819bed9f585d36f13a74093344d07a
Runolf <38182335+Runolf@users.noreply.github.com>
2020-02-13 11:48:48 +0100

    commit Add React project

---
+++ Front_Lisam_React/src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import App from './App';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+<App />, // appel le composant App décrit dans l'import 
+document.getElementById('root') //se trouve dans index.html
+);
+
