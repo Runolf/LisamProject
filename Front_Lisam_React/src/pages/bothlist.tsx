6ad80a3b4b6fd36b55df948229c8ce8d83198650
Runolf <38182335+Runolf@users.noreply.github.com>
2020-02-25 17:26:09 +0100

    some modif not important in bothlist

--- Front_Lisam_React/src/pages/bothlist.tsx
+++ Front_Lisam_React/src/pages/bothlist.tsx
@@ -1,13 +1,13 @@
-import {FunctionComponent} from 'react';
-// import CandP from '../component/client-and-project-card';
-// import Project from '../models/project';
-// import Client from '../models/client';
-// import ClientService from '../services/client-services';
+import React, {FunctionComponent, } from 'react';
+import CandP from '../component/client-and-project-card';
+import Project from '../models/project';
+import Client from '../models/client';
+import ClientService from '../services/client-services';
 import './list.css';
-// import { useProjects } from '../hooks/projects-hook';
+import { useProjects } from '../hooks/projects-hook';
 import { useClients } from '../hooks/clients-hook';
-// import { useClient } from '../hooks/client-hook';
-// import { useProject } from '../hooks/project-hook';
+import { useClient } from '../hooks/client-hook';
+import { useProject } from '../hooks/project-hook';
 
 
 export const BothList: FunctionComponent = () =>{
@@ -20,27 +20,19 @@ export const BothList: FunctionComponent = () =>{
         4) retourner l'objet
     */
 
-    // const GetProject = (idCLient: number): Project | null | undefined => {
-    //     //1 
-    //     var Cli = useClient(idCLient); 
-    //     //2
-    //      var FKProject:number|null|undefined = (Cli?.ProjectId !== undefined)? Cli.ProjectId :null;
-    //     //3
-    //      var Pro:Project|undefined|null = useProject(FKProject);
-    //     //4
-    //      return Pro;
+    const GetClient = (idCLient: number)/*: Client*/ => {
+        
+    }
 
-    // }
-
-    return ( null
-        // <div className="col grid">
-        //     { 
-        //         clients.map((C) => <CandP key={C.ClientId} client={C} project={
-        //                 GetProject(C.ClientId)
-        //         }/>)
-        //     }
+    return (
+        <div className="col grid">
+            {/* { 
+                clients.map((C) => <CandP key={C.ClientId} client={C} project={
+                        GetClient()
+                }/>)
+            } */}
             
-        // </div>
-    )
+        </div>
+    );
 } 
 
