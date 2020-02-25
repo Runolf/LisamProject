ea6634da93a2412be02ecbcbae623a3c97543c99
Runolf <38182335+Runolf@users.noreply.github.com>
2020-02-25 14:14:36 +0100

    add complete form for editing
    editing doesn't work yet

--- Front_Lisam_React/src/component/client-form.tsx
+++ Front_Lisam_React/src/component/client-form.tsx
@@ -80,17 +80,17 @@ const ClientForm: FunctionComponent<Props> = ({client, isEditForm}) => {
 
 
     return(
-        <form onSubmit={e => handleSubmit(e)}>
+        <form className="container" onSubmit={e => handleSubmit(e)}>
             <div>
-            <div>
-                    {isEditForm && (
-                        <div>
-                            <h3>Edit</h3>
-                            <span className="btn-floating halfway-fab waves-effect waves-light">
-                                <i className="material-icons" onClick={deleteClient}>delete</i>
-                            </span>
-                        </div>
-                    )}
+                <div>
+                        {isEditForm && (
+                            <div>
+                                <h3>Edit</h3>
+                                <span className="btn-floating right waves-effect waves-light">
+                                    <i className="material-icons" onClick={deleteClient}>delete</i>
+                                </span>
+                            </div>
+                        )}
                 </div>
 
                 <div>
@@ -99,6 +99,38 @@ const ClientForm: FunctionComponent<Props> = ({client, isEditForm}) => {
                     )}
                 </div>
             </div>
+
+            <div className="form-group">
+                    <label htmlFor="companyName">Company name</label>
+                    <input id="companyName" name="companyName" type="text" className="form-control" value={form.companyName.value} onChange={e => handleInputChange(e)}></input>        
+            </div>
+
+            <div className="form-group">
+                    <label htmlFor="email">E-mail</label>
+                    <input id="email" name="email" type="text" className="form-control" value={form.email.value} onChange={e => handleInputChange(e)}></input>        
+            </div>
+
+            <div className="form-group">
+                    <label htmlFor="number">number</label>
+                    <input id="number" name="number" type="text" className="form-control" value={form.number.value} onChange={e => handleInputChange(e)}></input>        
+            </div>
+            
+            <div className="form-group">
+                    <label htmlFor="language">language</label>
+                    <input id="language" name="language" type="text" className="form-control" value={form.language.value} onChange={e => handleInputChange(e)}></input>        
+            </div>
+            
+            <div className="form-group">
+                <h6>Adress</h6>
+                    <label htmlFor="city">city</label>
+                    <input id="city" name="city" type="text" className="form-control" value={form.city.value} onChange={e => handleInputChange(e)}></input> 
+                    <label htmlFor="zipCode">zip code</label>
+                    <input id="zipCode" name="zipCode" type="text" className="form-control" value={form.zipCode.value} onChange={e => handleInputChange(e)}></input>       
+                    <label htmlFor="street">street</label>
+                    <input id="street" name="street" type="text" className="form-control" value={form.street.value} onChange={e => handleInputChange(e)}></input>
+            </div>
+            
+            
                 
             <button type="submit" className="btn grey darken-3 waves-effect waves-black">Valider</button>
         </form>
--- Front_Lisam_React/src/services/client-services.ts
+++ Front_Lisam_React/src/services/client-services.ts
@@ -1,6 +1,7 @@
 import Client from '../models/client';
 
 export default class ClientService{ 
+  
     static getClients(): Promise<Client[]> {
         return fetch('http://localhost:44331/api/Client')
           .then(response => response.json())
@@ -45,6 +46,8 @@ export default class ClientService{
     .then(response => response.json())
     .catch(error => this.handleError(error));
 }
+
+
 
       static isEmpty(data: Object): boolean {
         return Object.keys(data).length === 0;
