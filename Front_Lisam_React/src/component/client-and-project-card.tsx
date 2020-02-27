d21b3244bb50c574cd6d9a3539f5c9ee3d88c951
Runolf <38182335+Runolf@users.noreply.github.com>
2020-02-27 10:26:38 +0100

    displaying bug of null clients FK fixed

--- Front_Lisam_React/src/component/client-and-project-card.tsx
+++ Front_Lisam_React/src/component/client-and-project-card.tsx
@@ -38,13 +38,22 @@ const BothCard: FunctionComponent<Props> = ({project,client}) => {
         //Nom Client | Numero projet | Date Sign | Project Leader |	Statut | Detail/edit/delete
 
         <tr className="" style={{color: "white"}}>
+            {client?
             <td className="grey darken-2 m2 center border" onClick={() => goToClient(client.ClientId)}>{client.Company_Name} </td>
+            : 
+            <td className="grey darken-2 m2 center border">No client assigned to this project</td>
+            }
+            
+            
+            
             <td className="grey darken-2 m2 center border" onClick={() => goToProject(project.ProjectId)}>{project.ProjectNumber}</td>
             <td className="grey darken-2 m2 center border">{project.SignatureDate.toString()
                                                                                                                                                   .slice(0, 10)
                                                                                                                                                   .split('-')
                                                                                                                                                   .join("/")}</td>
-            <td className="grey darken-2 m2 center border">{project.ProjectLeader}</td>
+            <td className="grey darken-2 m2 center border">{project?
+            (project.ProjectLeader):(<p>no project</p>)
+            }</td>
             <td className="grey darken-2 m2 center border">{st}</td>
             <td className="m2 td-button-list">
                 <tr  className="button-list btn green darken-4" onClick={() => goToProject(project.ProjectId)}>Detail project</tr>
